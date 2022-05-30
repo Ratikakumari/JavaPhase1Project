cc4e6bac5c954e14d564ebb15c8ac360fe752de1
Ratikakumari <102657117+Ratikakumari@users.noreply.github.com>
2022-05-30 10:07:04 +0530

    Add files via upload

---
+++ springweb/src/main/java/com/example/demo/Display.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+public class Display {
+
+public String display() {
+		return "hi simplilearn users";
+}}
